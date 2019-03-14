7d7d94d60756322ba3b8bf5b164579e6d5705fbb
Vishal Khanna <vishalkhanna2906@gmail.com>
2019-03-14 17:44:38 +1100

    changes for urbanplanning ai

--- my-mapbox.js
+++ my-mapbox.js
@@ -39,22 +39,75 @@ function MapGL() {
   // initialize map
   var newMap = new mapboxgl.Map({
       container: "map", // container id
-      style: "mapbox://styles/mapbox/streets-v9", //stylesheet location
-      center: [-77.04, 38.907], // starting position
-      zoom: 11 // starting zoom
+      // style: "mapbox://styles/mapbox/streets-v9", //stylesheet location
+      style: 'mapbox://styles/mapbox/light-v9',
+      // center: [-37.809820, 144.96983],
+
+      center: [ 144.96983,-37.809820],
+      zoom: 15.5,
+      pitch: 45,
+      bearing: -0,
   });
 
   // geocoding
   newMap.addControl(new MapboxGeocoder({
     accessToken: mapboxgl.accessToken
   }));
+
+
+
+
+
   
   // event handlers
   newMap.on("load", mapLoaded);
-    return newMap;
+
+
+  return newMap;
 }
 
 function mapLoaded() {
   // do stuff here
+
+// Insert the layer beneath any symbol layer.
+var layers = map.getStyle().layers;
+ 
+var labelLayerId;
+for (var i = 0; i < layers.length; i++) {
+if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+labelLayerId = layers[i].id;
+break;
+}
+}
+ 
+
+  map.addLayer({
+    'id': '3d-buildings',
+    'source': 'composite',
+    'source-layer': 'building',
+    'filter': ['==', 'extrude', 'true'],
+    'type': 'fill-extrusion',
+    'minzoom': 15,
+    'paint': {
+    'fill-extrusion-color': '#aaa',
+     
+    // use an 'interpolate' expression to add a smooth transition effect to the
+    // buildings as the user zooms in
+    'fill-extrusion-height': [
+    "interpolate", ["linear"], ["zoom"],
+    15, 0,
+    15.05, ["get", "height"]
+    ],
+    
+    'fill-extrusion-base': [
+    "interpolate", ["linear"], ["zoom"],
+    15, 0,
+    15.05, ["get", "min_height"]
+    ],
+
+    'fill-extrusion-opacity': .6
+    }
+    }, labelLayerId);
+
   
 }
